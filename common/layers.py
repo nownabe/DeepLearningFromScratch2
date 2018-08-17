3146f868f68d359bf577e948486fea804755816a
nownabe <nownabe@gmail.com>
2018-08-17 23:57:20 +0900

    1.3.5.2

--- common/layers.py
+++ common/layers.py
@@ -35,3 +35,26 @@ class Sigmoid:
     def backwward(self, dout):
         dx = dout(1.0 - self.out) * self.out
         return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grands = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
