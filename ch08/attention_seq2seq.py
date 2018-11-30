1289482ff3e197c2f12f01ab3bd5b2d35e90e200
nownabe <nownabe@gmail.com>
2018-11-30 20:41:18 +0900

    8.2.3

--- ch08/attention_seq2seq.py
+++ ch08/attention_seq2seq.py
@@ -88,3 +88,12 @@ class AttentionDecoder:
         return sampled
 
 
+class AttentionSeq2seq(Seq2seq):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        args = vocab_size, wordvec_size, hidden_size
+        self.encoder = AttentionEncoder(*args)
+        self.decoder = AttentionDecoder(*args)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
